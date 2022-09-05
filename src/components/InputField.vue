d5f175ca732a7d5f73e61f2320e69d0c814e7471
Ismail Karabulut <i.karabulut93@gmail.com>
2022-09-05 11:30:44 -0500

    adds component for input field

---
+++ src/components/InputField.vue
@@ -0,0 +1,73 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'InputField',
+  components: {},
+  data() {
+    return {
+      optimistic: 0,
+      pessimistic: 0,
+      mostLikely: 0,
+      clicked: false as boolean,
+    }
+  },
+  methods: {
+    generateEstimate() {
+      this.clicked = true
+      console.log('hello', this.optimistic, this.pessimistic, this.mostLikely)
+    },
+  },
+})
+</script>
+
+<template>
+  <div class="row gx-4 gx-lg-5 mx-4 my-4">
+    <div class="col-md-4">
+      <div data-test="optimisticCard" class="card">
+        <div class="card-body">
+          <h2 class="card-title">Optimistic</h2>
+        </div>
+        <div class="card-footer">
+          <input type="text" v-model="optimistic" />
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div data-test="mostLikelyCard" class="card">
+        <div class="card-body">
+          <h2 class="card-title">Most Likely</h2>
+        </div>
+        <div class="card-footer">
+          <input type="text" v-model="mostLikely" />
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div data-test="pessimisticCard" class="card">
+        <div class="card-body">
+          <h2 class="card-title">Pessimistic</h2>
+        </div>
+        <div class="card-footer">
+          <input type="text" v-model="pessimistic" />
+        </div>
+      </div>
+    </div>
+  </div>
+  <button
+    data-test="calculateButton"
+    type="button"
+    @click="generateEstimate()"
+    class="btn btn-primary mt-2 mb-4"
+  >
+    Calculate Estimate
+  </button>
+  <button
+    data-test="saveButton"
+    v-show="clicked"
+    type="submit"
+    class="btn btn-outline-success btn-sm"
+  >
+    Save?
+  </button>
+</template>
---
+++ src/components/__tests__/InputField.spec.ts
@@ -0,0 +1,53 @@
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import InputField from '../InputField.vue'
+
+describe('InputField', () => {
+  it('should take in an Optimistic estimate', () => {
+    const wrapper = mount(InputField)
+    const optimisticCard = wrapper.get('[data-test="optimisticCard"]')
+    const input = optimisticCard.find('input')
+
+    input.setValue('743')
+
+    expect(input.element.value).toBe('743')
+  })
+
+  it('should take in a Most Likely estimate', () => {
+    const wrapper = mount(InputField)
+    const mostLikelyCard = wrapper.get('[data-test="mostLikelyCard"]')
+    const mostLikelyInput = mostLikelyCard.find('input')
+
+    mostLikelyInput.setValue('8754')
+
+    expect(mostLikelyInput.element.value).toBe('8754')
+  })
+
+  it('should take in a pessimistic estimate', () => {
+    const wrapper = mount(InputField)
+    const pessimisticCard = wrapper.get('[data-test="pessimisticCard"]')
+    const pessimisticInput = pessimisticCard.find('input')
+
+    pessimisticInput.setValue('2343')
+
+    expect(pessimisticInput.element.value).toBe('2343')
+  })
+
+  it('should display a button to calculate the estimate', () => {
+    const wrapper = mount(InputField)
+    const calculateButton = wrapper.findAll('[data-test="calculateButton"]')
+
+    expect(calculateButton).toHaveLength(1)
+  })
+
+  it('should display a save button when the calculate button is pressed', () => {
+    const wrapper = mount(InputField)
+    const calculateButton = wrapper.get('[data-test="calculateButton"]')
+
+    calculateButton.trigger('click')
+    const saveButton = wrapper.findAll('[data-test="saveButton"]')
+
+    expect(saveButton).toHaveLength(1)
+  })
+})
