54ed3f4ac57d2833bb8b39ebc5c6ce959238c30b
Ismail Karabulut <i.karabulut93@gmail.com>
2022-09-05 11:44:40 -0500

    fixes merge conflict

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
 import NavBar from '../components/NavBar.vue'
+import EstimateSelectors from '../components/EstimateSelectors.vue'
+import InputField from '../components/InputField.vue'
 </script>
 
 <template>
   <main>
     <NavBar />
+
+    <div class="card text-white bg-secondary my-4">
+      <div class="card-body">
+        <p class="text-white m-0">Select Your Estimate Type</p>
+      </div>
+    </div>
+
+    <EstimateSelectors />
+    <InputField />
+
+    <footer class="py-3 bg-dark">
+      <div class="container px-4 px-lg-5 bg-dark">
+        <p class="text-center text-white">Copyright &copy; Mavi 2022</p>
+      </div>
+    </footer>
   </main>
 </template>
